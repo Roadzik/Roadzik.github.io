afd0cabdd028b00349fadca9534107cee026ee78
sobo32 <Roadzik66@gmail.com>
2024-01-09 19:25:52 +0100

    Commit message

---
+++ partnership.js
@@ -0,0 +1,3 @@
+const partnership = document.getElementById("partnership");
+
+partnership.innerHTML = "<a href=\"https://teoriaprawko.pl/\"><img src=\"https://praktycznyegzamin.pl/img/teoriaprawko-1.jpg\" alt=\"teoria prawko pl partnership\"</a>"
